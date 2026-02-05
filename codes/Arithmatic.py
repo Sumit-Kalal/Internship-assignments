ed6bff674c271197a5a3d710d2ce171a0076562f
Sumit Kalal <sumitkalal128@gmail.com>
2026-02-05 19:26:14 +0530

    initial commit

---
+++ codes/Arithmatic.py
@@ -0,0 +1,8 @@
+a=float(input("Enter first number: "))
+b=float(input("Enter second number: "))
+sum = a + b
+diff = a - b
+mul = a * b
+div = a / b
+power = a ** b
+print("Operations on the numbers, Sum =",sum, "difference = ", diff, "Multiplication =", mul, "division =", div, "power = ", power)
---
+++ codes/Introduction.py
@@ -0,0 +1,9 @@
+print ("Name: Sumit Kalal")
+print ("DOB:05/11/2006")
+print ("Address: Siddrameshwar nagar, Unkal, Hubli.")
+print ("Education: Bachelor of Science (ongoing)")
+print ("Institute: PC Jabin Science College, Hubli.")
+print ("Role: Project Manager intern at AXILEX group pf companies.")
+print ("Contact: 7022730028")
+print ("Email:sumitkalal128@gmail.com")
+
