4df5c7284dcabd33588a0e41965490acd6c71cd7
Sumit Kalal <sumitkalal128@gmail.com>
2026-02-06 08:40:52 +0530

    more code

---
+++ Result determination.py
@@ -0,0 +1,9 @@
+marks=float(input("Enter your marks: "))
+if marks>80:
+    print("You have scored distinction.")
+elif marks>60:
+    print("You have secured first class.")
+elif marks>40:
+    print("You have passed.")
+else:
+    print("You have failed, better luck next time.")
