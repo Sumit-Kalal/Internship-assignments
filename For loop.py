95db4293f6e0a47e1213e5397a951741b1eadd01
Sumit Kalal <sumitkalal128@gmail.com>
2026-02-07 05:30:18 +0530

    More code

---
+++ For loop.py
@@ -0,0 +1,7 @@
+#Program to calculate the sum of all the multiples of 3 from 1 to n using for loop.
+n=int(input("Enter a number: "))
+sum=0
+for i in range(1, n+1):
+    if i%3==0:
+        sum=sum+i
+print(f"The sum of all the multiples of 3 from 1 to {n} is: {sum}")
---
+++ While loop.py
@@ -0,0 +1,9 @@
+# Program to test the Collatz conjecture.
+n=int(input("Enter any number: "))
+while n!=1:
+    print(n)
+    if n%2==0:
+        n=n//2
+    else:
+        n=3*n+1
+print(1)
