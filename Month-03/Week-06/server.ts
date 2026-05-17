56bc4fa5015bfb7b44b80e4a2e33130b8dd998b9
Sumit Kalal <sumitkalal128@gmail.com>
2026-05-17 06:28:02 +0530

    Fixes for render

--- Month-03/Week-06/server.ts
+++ Month-03/Week-06/server.ts
@@ -73,7 +73,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/payments', paymentRoutes);
 
-app.get('/', (_req, res) => {
+app.get('/health', (_req, res) => {
   res.send('Server is running');
 });
 
